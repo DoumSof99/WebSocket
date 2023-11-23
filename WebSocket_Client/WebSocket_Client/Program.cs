5dc0b75baabc3747afc6d169962d73682d99d51a
DoumSof99 <sofiadmn99@gmail.com>
2023-11-23 20:52:25 +0200

    start websocket

--- WebSocket_Client/WebSocket_Client/Program.cs
+++ WebSocket_Client/WebSocket_Client/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using WebSocketSharp;
+
 namespace WebSocket_Client
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // create an instance of a websocket client
+            WebSocket ws = new WebSocket("");
+
+            Console.ReadKey();
         }
     }
 }
